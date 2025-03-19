f78a3f12a3bf964b5572cdcfc82f3d3f3367da2d
albrinBuzz <152460564+albrinBuzz@users.noreply.github.com>
2025-03-18 21:13:58 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyD5i82Kh1WRZxhoDpy-ti3gXT0TVuslRl8",
-    authDomain: "nexxus-80f29.firebaseapp.com",
-    projectId: "nexxus-80f29",
-    storageBucket: "nexxus-80f29.firebasestorage.app",
-    messagingSenderId: "842534189784",
-    appId: "1:842534189784:web:be6b4e2cee3b9f1a9435ad"
+    apiKey: "AIzaSyALQcW-IFcaNEk5_eAcB_OE1qKBGD6hsjI",
+    authDomain: "nexxuxapp.firebaseapp.com",
+    projectId: "nexxuxapp",
+    storageBucket: "nexxuxapp.firebasestorage.app",
+    messagingSenderId: "734283185222",
+    appId: "1:734283185222:web:2bd33bb0dee3fa45335e47"
   }
 
 };
